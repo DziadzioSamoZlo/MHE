6149eda3388e611b2b21a242790cf74eec56daa2
DziadzioSamoZlo <48621068+DziadzioSamoZlo@users.noreply.github.com>
2020-06-19 21:15:54 +0200

    Metaheurystics
    Working Bruteforce with time measurement

--- PostCorrespondenceProblem.py
+++ PostCorrespondenceProblem.py
@@ -1,9 +1,9 @@
 import json
+import time
 
-data = json.load(open("test.json", 'r'))
+data = json.load(open("testEasy.json", 'r'))
 working = json.load(open("working.json", 'r'))
 
-#Dążymy do tego, żeby wartość była jak największa
 def block_value(first, second):
     value = 0
     for i in range(0, min(len(first), len(second))):
@@ -22,7 +22,6 @@ def best_block_to_add():
         firstsqnc = baseBlock[0] + checkedBlock[0]
         secondsqnc = baseBlock[1] + checkedBlock[1]
         checkedBlockValue = block_value(firstsqnc, secondsqnc)
-        #print("sprawdzany: ", firstsqnc, " i ", secondsqnc, " wartość: ", checkedBlockValue)
         if checkedBlockValue == 0:
             howMuchBestBlocks += 1
             if howMuchBestBlocks > 1:
@@ -33,19 +32,23 @@ def best_block_to_add():
             return 0
     return bestPair
 
+#Aby Brute Force byl zgodny z definicja, rozwiazywany problem 
+#musi byc w jakis sposob ograniczony, dlatego też używamy limitu
+#użytych bloczków
 def best_block_to_add_with_limit(maxOccurs = 22):
     howMuchBestBlocks = 0
     #pobieramy aktualny stan bloczków
     baseBlock = working.get(str("workingBlocks"))
 
+    #pobieramy nowy bloczek do dodania
     for toCheck in range(1,5):
-        #pobieramy nowy bloczek do dodania
+        numberOfOccurs = 0
         checkedBlock = data.get(str(toCheck))
         firstsqnc = baseBlock[0] + checkedBlock[0]
         secondsqnc = baseBlock[1] + checkedBlock[1]
         checkedBlockValue = block_value(firstsqnc, secondsqnc)
         newSequence = working.get("sequence") + str(toCheck)
-        numberOfOccurs = 0
+        #sprawdzamy ile razy nowy bloczek został użyty:
         for a in newSequence:
             if a == str(toCheck):
                 numberOfOccurs += 1
@@ -66,15 +69,20 @@ def add_next_block(whichBlock):
     changedJson = json.load(open("working.json", 'r'))
     blockToAdd = data.get(str(whichBlock))
     baseBlock = working.get("workingBlocks")
+    blocksUsed = working.get("blocksAdded")
     
     #dodanie nowego bloczka do całego jsona working
     baseBlockFirst = baseBlock[0] + blockToAdd[0]
     baseBlockSecond = baseBlock[1] + blockToAdd[1]
     baseSequence = working.get("sequence") + str(whichBlock)
     
+    #sprawdzenie czy wszystkie bloczki zostały użyte
+    if whichBlock not in blocksUsed:
+        blocksUsed.append(whichBlock)
+        
     #tworzenie zmienionego jsona
     changedJson["workingBlocks"] = [baseBlockFirst, baseBlockSecond]
-    changedJson["lastAdded"] = blockToAdd
+    changedJson["blocksAdded"] = blocksUsed
     changedJson["sequence"] = baseSequence
     with open("working.json", 'w') as changeWorkingJson:
         json.dump(changedJson, changeWorkingJson)
@@ -85,42 +93,14 @@ def add_next_block(whichBlock):
 def add_checkpoint(checkpointSequence):
     changedJson = json.load(open("working.json", 'r'))
     changedJson["checkpoint"].append(checkpointSequence)
-    #print("Adding checkpoint: ", checkpointSequence)
     with open("working.json", 'w') as changeWorkingJson:
         json.dump(changedJson, changeWorkingJson)
     refresh_working_json()
 
 def go_to_last_checkpoint():
-    #print("Reversing last sequence.")
     changedJson = json.load(open("working.json", 'r'))
     checkpoints = changedJson["checkpoint"]
     lastCheckpoint = checkpoints.pop(0)
-    reversedBlockFirst = ""
-    reversedBlockSecond = ""
-    for i in lastCheckpoint:
-        block = data.get(str(i))
-        reversedBlockFirst = reversedBlockFirst + block[0]
-        reversedBlockSecond = reversedBlockSecond + block[1]
-    changedJson["workingBlocks"][0] = reversedBlockFirst
-    changedJson["workingBlocks"][1] = reversedBlockSecond
-    changedJson["checkpoint"] = checkpoints
-    changedJson["sequence"] = lastCheckpoint
-
-    with open("working.json", 'w') as changeWorkingJson:
-        json.dump(changedJson, changeWorkingJson)
-    refresh_working_json()
-
-def go_to_last_checkpoint_test():
-    changedJson = json.load(open("working.json", 'r'))
-    killCheckpoint = changedJson["sequence"]
-    checkpoints = changedJson["checkpoint"]
-    checkpointsToKill = [s for s in checkpoints if killCheckpoint in s]
-    if len(checkpointsToKill) == 0:
-        lastCheckpoint = checkpoints.pop(0)
-    else:
-        checkpoints = [i for i in checkpoints if i not in checkpointsToKill]
-        lastCheckpoint = checkpoints.pop(0)
-    
     reversedBlockFirst = ""
     reversedBlockSecond = ""
     for i in lastCheckpoint:
@@ -142,41 +122,32 @@ def refresh_working_json():
 
 def clear_working_json():
     global working
-    clearJson = {"workingBlocks": ["", ""], "lastAdded": ["", ""], "checkpoint": [], "sequence": ""}
+    clearJson = {"workingBlocks": ["", ""], "blocksAdded": [], "checkpoint": [], "sequence": ""}
     with open("working.json", 'w') as changeWorkingJson:
         json.dump(clearJson, changeWorkingJson)
     refresh_working_json()
+    
+def measure_time(function, limit):
+    start = time.perf_counter()
+    function(limit)
+    end = time.perf_counter()
+    print(function.__name__, "took:", end-start, "s")
 
-def checkIfSimilar(test):
-    res = None
-    for i in range(1, len(test)//2 + 1): 
-        if (not len(test) % len(test[0:i]) and test[0:i] *
-            (len(test)//len(test[0:i])) == test): 
-            res = test[0:i] 
-    return res
-
-def Bruteforce():
+def Bruteforce(limit = 22):
+    clear_working_json()
     for i in range(0,20000):
-        #print("najlepszy możliwy bloczek:")
-        if working.get("sequence") == data.get("sol"):
+        if working["workingBlocks"][0] == working["workingBlocks"][1] and i > 0 and len(working["blocksAdded"]) == 4:
             print("solution found on iteration: ", i)
             break
-        added = best_block_to_add_with_limit()
+        added = best_block_to_add_with_limit(limit)
         if added == 0:
             go_to_last_checkpoint()
             continue
-        #print(added)
-        #print("dodawany bloczek:")
-        #print(data.get(str(added)))
         add_next_block(added)
     print("Brute Force found:")
     print(working["sequence"])
     print("Solution:")
     print(data["sol"])
 
-clear_working_json()
-Bruteforce()
-#print(working.get("sequence")[7:])
-#test()
-#test_new_checkpoint_method()
 #go_to_last_checkpoint_test()
+measure_time(Bruteforce, 2)
